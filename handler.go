0d540bcc8a065861bdab24f1b27394831de872aa
Cody Craven <cody@cravencode.com>
2022-11-29 01:50:09 -0800

    Attempt to fix code example indentations

--- handler.go
+++ handler.go
@@ -27,12 +27,12 @@ func Trap(fn SignalHandler, s ...os.Signal) {
 // Message creates a SignalHandler that writes to an io.Writer and then chains
 // to another SignalHandler.
 //
-//  grip.Trap(
-//      grip.Message("received shutdown request", os.Stdout, func(_ os.Signal) {
-//          fmt.Println("our signal handler")
-//      }),
-//      syscall.SIGINT, syscall.SIGTERM,
-//  )
+//	grip.Trap(
+//		grip.Message("received shutdown request", os.Stdout, func(_ os.Signal) {
+//			fmt.Println("our signal handler")
+//		}),
+//		syscall.SIGINT, syscall.SIGTERM,
+//	)
 func Message(m string, w io.Writer, fn SignalHandler) SignalHandler {
 	return func(s os.Signal) {
 		fmt.Fprintf(w, "%s: %s\n", m, s)
@@ -56,53 +56,53 @@ func Message(m string, w io.Writer, fn SignalHandler) SignalHandler {
 //
 // A complete example:
 //
-//  package main
+//	package main
 //
-//  import (
-//      "fmt"
-//      "os"
-//      "syscall"
+//	import (
+//		"fmt"
+//		"os"
+//		"syscall"
 //
-//      "github.com/codycraven/grip"
-//  )
+//		"github.com/codycraven/grip"
+//	)
 //
-//  func main() {
-//      // Create a channel to receive the exit code.
-//      ch := make(chan int)
-// 	    grip.Trap(
-//          // Passing to Message isn't needed, added just to show its use.
-//          grip.Message("received shutdown request", os.Stdout, grip.Exit(
-//              // The channel for Exit to send the exit code to.
-//              ch,
-//              // The io.Writer for Exit to write received errors to.
-//              os.Stderr,
-//              // ExitHandlers that should run (in sequential order).
-//              func() error {
-//                  // A graceful shutdown step, if it returned an error it
-//                  // would add 1 to the exit code.
-//                  return nil
-//              },
-//              func() error {
-//                  // Another graceful shutdown step.
-//                  return fmt.Errorf("since this is the second Exit handler it'll add 2")
-//              },
-//              func() error {
-//                  // Yet another graceful shutdown step.
-//                  return fmt.Errorf("since this is the third Exit handler it'll add 4")
-//              },
-//              func() error {
-//                  // Our last graceful shutdown step, if it returned an error
-//                  // it would add 8 to the exit code.
-//                  return nil
-//              },
-//          )),
-//          // Listen for these signals:
-// 	        syscall.SIGINT,
-//          syscall.SIGTERM,
-//      )
-//      // Wait on channel to receive int before exiting.
-//      os.Exit(<-ch)
-//  }
+//	func main() {
+//		// Create a channel to receive the exit code.
+//		ch := make(chan int)
+//		grip.Trap(
+//			// Passing to Message isn't needed, added just to show its use.
+//			grip.Message("received shutdown request", os.Stdout, grip.Exit(
+//				// The channel for Exit to send the exit code to.
+//				ch,
+//				// The io.Writer for Exit to write received errors to.
+//				os.Stderr,
+//				// ExitHandlers that should run (in sequential order).
+//				func() error {
+//					// A graceful shutdown step, if it returned an error it
+//					// would add 1 to the exit code.
+//					return nil
+//				},
+//				func() error {
+//					// Another graceful shutdown step.
+//					return fmt.Errorf("since this is the second Exit handler it'll add 2")
+//				},
+//				func() error {
+//					// Yet another graceful shutdown step.
+//					return fmt.Errorf("since this is the third Exit handler it'll add 4")
+//				},
+//				func() error {
+//					// Our last graceful shutdown step, if it returned an error
+//					// it would add 8 to the exit code.
+//					return nil
+//				},
+//			)),
+//			// Listen for these signals:
+//			syscall.SIGINT,
+//			syscall.SIGTERM,
+//		)
+//		// Wait on channel to receive int before exiting.
+//		os.Exit(<-ch)
+//	}
 func Exit(ch chan int, errWriter io.Writer, fn ...ExitHandler) SignalHandler {
 	return func(s os.Signal) {
 		exit := 0
